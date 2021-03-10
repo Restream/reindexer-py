434bba189db7819c2692a6c45f727d621720c86f
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-03-11 00:19:04 +0300

    github actions; fix test

--- .github/workflows/install_reindexer_ubuntu_focal.sh
+++ .github/workflows/install_reindexer_ubuntu_focal.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+filelist=$(wget -q -O - http://repo.reindexer.org/ubuntu-focal/amd64/ | grep "reindexer-dev" | sed "/.*>\(reindexer-dev.*\)<.*/s//\1/")
+
+filename=$(./.github/workflows/choose_latest_version.sh "$filelist")
+
+wget -O $filename http://repo.reindexer.org/ubuntu-focal/amd64/$filename
+sudo dpkg -i $filename
+sudo apt-get install -f
+
 filelist=$(wget -q -O - http://repo.reindexer.org/ubuntu-focal/amd64/ | grep "reindexer-server" | sed "/.*>\(reindexer-server.*\)<.*/s//\1/")
 
 filename=$(./.github/workflows/choose_latest_version.sh "$filelist")
