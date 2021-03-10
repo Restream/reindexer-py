752c9cea117bd2169043942550c4bd2c92c0c8ad
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-03-10 21:12:51 +0300

    github actions; fix tests

--- .github/workflows/install_reindexer_macos.sh
+++ .github/workflows/install_reindexer_macos.sh
@@ -2,7 +2,7 @@
 
 filelist=$(wget -q -O - http://repo.reindexer.org/brew-bottles/ | grep "reindexer-" | sed "/.*>\(reindexer-.*\)<.*/s//\1/")
 
-filename=$(./choose_latest_version.sh "$filelist")
+filename=$(./.github/workflows/choose_latest_version.sh "$filelist")
 
 wget -O $filename http://repo.reindexer.org/brew-bottles/$filename
 brew tap restream/reindexer
--- .github/workflows/install_reindexer_ubuntu_bionic.sh
+++ .github/workflows/install_reindexer_ubuntu_bionic.sh
@@ -2,7 +2,7 @@
 
 filelist=$(wget -q -O - http://repo.reindexer.org/ubuntu-bionic/amd64/ | grep "reindexer-dev" | sed "/.*>\(reindexer-dev.*\)<.*/s//\1/")
 
-filename=$(./choose_latest_version.sh "$filelist")
+filename=$(./.github/workflows/choose_latest_version.sh "$filelist")
 
 wget -O $filename http://repo.reindexer.org/ubuntu-bionic/amd64/$filename
 apt install $filename
