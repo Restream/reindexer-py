438df1b62865328915ea97b74f3e5b224f036761
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-03-10 11:23:42 +0300

    github actions; rx install script

---
+++ .github/workflows/install_reindexer.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+filelist=$(wget -q -O - http://repo.reindexer.org/brew-bottles/ | grep "reindexer-" | sed "/.*>\(reindexer-.*\)<.*/s//\1/")
+ver1=0
+ver2=0
+ver3=0
+for x in $filelist
+do
+	v1=$(echo $x | sed "/[^0-9]*\([0-9]*\)\..*/s//\1/")
+	v2=$(echo $x | sed "/[^\.]*\.\([0-9]*\)\..*/s//\1/")
+	v3=$(echo $x | sed "/[^\.]*\.[0-9]*\.\([0-9]\)\..*/s//\1/")
+	if [[ $v1 -gt $ver1 || ( $v1 -eq $ver1 && ( $v2 -gt $ver2 || ( $v2 -eq $ver2 && $v3 -gt $ver3 ) ) ) ]]
+	then
+		ver1=$v1
+		ver2=$v2
+		ver3=$v3
+		filename=$x
+	fi
+done
+wget -O $filename http://repo.reindexer.org/brew-bottles/$filename
+brew install $filename
