6838b4c5399636ea6dda51cd3c564ede192a2aa0
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-03-10 17:34:28 +0300

    github actions; add osx test

---
+++ .github/workflows/test_pyreindexer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+python3 example/main.py
+cd tests
+python3 -m unittest test_builtin.py
+reindexer_server --db /tmp/reindex_test -l0 --serverlog="" --corelog="" --httplog="" --rpclog="" &
+server_pid=$!
+sleep 1
+python3 -m unittest test_cproto.py
+
+kill $server_pid
+wait $server_pid
