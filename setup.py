74168028b826608310db82271e91db4608fb44a0
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-03-11 09:45:43 +0300

    github actions; fix test

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class build_ext(build_ext_orig):
 
 
 setup(name=PACKAGE_NAME,
-      version='0.1.63',
+      version='0.1.64',
       description='A connector that allows to interact with Reindexer',
       url='https://github.com/Restream/pyreindexer',
       author='Igor Tulmentyev',
