0c5dbc10cd83d7f71afde7440f08a360d7b00141
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-03-11 08:09:11 +0300

    github actions; fix test

--- .github/workflows/test_pyreindexer.sh
+++ .github/workflows/test_pyreindexer.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-pwd
-ls
-python3 example/main.py
+echo $(python3 example/main.py)
 cd tests
 python3 -m unittest test_builtin.py
 reindexer_server --db /tmp/reindex_test -l0 --serverlog="" --corelog="" --httplog="" --rpclog="" &
