3c8b7effc0fbf4d907f0d086feba777b0ae03f88
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-03-15 15:36:28 +0300

    gh actions; standart rx install

--- .github/workflows/install_reindexer_macos.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-filelist=$(wget -q -O - http://repo.reindexer.org/brew-bottles/ | grep "reindexer-" | sed "/.*>\(reindexer-.*\)<.*/s//\1/")
-
-filename=$(./.github/workflows/choose_latest_version.sh "$filelist")
-
-wget -O $filename http://repo.reindexer.org/brew-bottles/$filename
-brew tap restream/reindexer
-brew install $filename
