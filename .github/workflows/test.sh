c438913da0f8164f67b222e2190a86bd05905f60
Sannikov Ilya <Ilya.I.Sannikov@rt.ru>
2021-03-15 16:16:44 +0300

    gh actions; standart rx install

---
+++ .github/workflows/test.sh
@@ -0,0 +1,8 @@
+python3 example/main.py || exit 1
+python3 -m unittest tests/test_builtin.py || exit 1
+# reindexer_server --db /tmp/reindex_test -l0 --serverlog="" --corelog="" --httplog="" --rpclog="" &
+# server_pid=$!
+sleep 1
+python3 -m unittest tests/test_cproto.py || exit 1
+kill $server_pid
+wait $server_pid
--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class build_ext(build_ext_orig):
 
 
 setup(name=PACKAGE_NAME,
-      version='0.1.67',
+      version='0.1.68',
       description='A connector that allows to interact with Reindexer',
       url='https://github.com/Restream/pyreindexer',
       author='Igor Tulmentyev',
