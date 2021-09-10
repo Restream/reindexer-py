6f1f71fbad7bcb313dc12e0a6c9edbb46b7a8964
T.Radounia <t.radounia@andersenlab.com>
2021-09-10 16:21:29 +0300

    change version of pyrx to 0.2.2

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class build_ext(build_ext_orig):
 
 
 setup(name=PACKAGE_NAME,
-      version='0.2.1',
+      version='0.2.2',
       description='A connector that allows to interact with Reindexer',
       url='https://github.com/Restream/reindexer-py',
       author='Igor Tulmentyev',
