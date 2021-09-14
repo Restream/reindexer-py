f1619265a2df4b160928086f23ddd2743391863a
Alexandr Shelepin <57798122+graveart@users.noreply.github.com>
2021-09-14 16:12:06 +0300

    [fix] Move qa_test into tests

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,8 +1,8 @@
 python3 example/main.py || exit 1
-python3 -m pytest ../qa_test/tests || exit 1
+python3 -m pytest tests/tests || exit 1
 reindexer_server --db /tmp/reindex_test -l0 --serverlog="" --corelog="" --httplog="" --rpclog="" &
 server_pid=$!
 sleep 1
-python3 -m pytest ../qa_test/tests --mode=cproto || exit 1
+python3 -m pytest tests/tests --mode=cproto || exit 1
 kill $server_pid
 wait $server_pid
