169056c55efaf25cee9a566fcfa26d428d5b29a5
NikitaEvgen <bne8081@gmail.com>
2024-11-08 14:33:54 +0300

    [ref] refactor tests (#15)
    Refactor tests
    
    ---------
    
    Co-authored-by: Nikita Bushmakin <busn@exante.eu>

--- pyreindexer/tests/tests/test_namespace.py
+++ pyreindexer/tests/tests/test_namespace.py
@@ -1,36 +1,33 @@
 from hamcrest import *
 
-from tests.helpers.namespace import *
-
 
 class TestCrudNamespace:
-    def test_create_ns(self, database):
-        # Given("Create namespace in empty database")
-        namespace_name = 'test_ns'
-        create_namespace(database, namespace_name)
-        # When ("Get namespaces list in created database")
-        namespace_list = get_namespace_list(database)
-        # Then ("Check that database contains created namespace")
-        assert_that(namespace_list, has_item(has_entry("name", equal_to(namespace_name))),
-                    "Namespace wasn't created")
-        drop_namespace(database, namespace_name)
 
-    def test_delete_ns(self, database):
-        # Given("Create namespace")
-        namespace_name = 'test_ns'
-        create_namespace(database, namespace_name)
+    def test_create_ns(self, db):
+        # Given("Create namespace in empty database")
+        namespace_name = 'test_ns1'
+        db.namespace.open(namespace_name)
+        # When ("Get namespaces list in created database")
+        namespace_list = db.namespace.enumerate()
+        # Then ("Check that database contains created namespace")
+        assert_that(namespace_list, has_item(has_entries(name=namespace_name)),
+                    "Namespace wasn't created")
+        db.namespace.drop(namespace_name)
+
+    def test_delete_ns(self, db):
+        # Given("Create namespace in empty database")
+        namespace_name = 'test_ns2'
+        db.namespace.open(namespace_name)
         # When ("Delete namespace")
-        drop_namespace(database, namespace_name)
+        db.namespace.drop(namespace_name)
         # Then ("Check that namespace was deleted")
-        namespace_list = get_namespace_list(database)
-        assert_that(namespace_list, not_(has_item(has_entry("name", equal_to(namespace_name)))),
+        namespace_list = db.namespace.enumerate()
+        assert_that(namespace_list, not_(has_item(has_entries(name=namespace_name))),
                     "Namespace wasn't deleted")
 
-    def test_cannot_delete_ns_not_created(self, database):
-        # Given ("Create empty database")
+    def test_cannot_delete_ns_not_created(self, db):
+        # Given ("Created empty database")
         # Then ("Check that we cannot delete namespace that does not exist")
         namespace_name = 'test_ns'
-        assert_that(calling(drop_namespace).with_args(database, namespace_name), raises(Exception, matching=has_string(
-            f"Namespace '{namespace_name}' does not exist")))
-
-
+        assert_that(calling(db.namespace.drop).with_args(namespace_name),
+                    raises(Exception, pattern=f"Namespace '{namespace_name}' does not exist"))
