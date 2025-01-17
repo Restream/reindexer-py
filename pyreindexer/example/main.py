fbb22b4cb25cf20f0027b9a8fd21d2eaa7951dd1
Nikita Bushmakin <nikita.bushmakin@restream.ru>
2025-01-17 13:07:58 +0300

    add options tests

--- pyreindexer/example/main.py
+++ pyreindexer/example/main.py
@@ -57,10 +57,10 @@ def create_items_example(db, namespace):
 def select_item_query_example(db, namespace):
     item_name_for_lookup = 'item_0'
 
-    return (db.select("SELECT * FROM " + namespace + " WHERE name='" + item_name_for_lookup + "'", timedelta(milliseconds = 1000)))
+    return db.select(f"SELECT * FROM {namespace} WHERE name='{item_name_for_lookup}'", timedelta(milliseconds = 1000))
 
 def select_all_item_query_example(db, namespace):
-    return db.select("SELECT * FROM " + namespace, timedelta(milliseconds = 1000))
+    return db.select(f"SELECT * FROM {namespace}", timedelta(milliseconds = 1000))
 
 def print_all_records_from_namespace(db, namespace, message):
     selected_items_tr = select_all_item_query_example(db, namespace)
