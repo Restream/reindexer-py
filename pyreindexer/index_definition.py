50d763ea256e96027b7c09550725aec5c3e40205
Alexander.A,Utkin <aleksandr.utkin@restream.ru>
2025-02-27 10:01:41 +0300

    Update link to readme

--- pyreindexer/index_definition.py
+++ pyreindexer/index_definition.py
@@ -27,7 +27,7 @@ class IndexDefinition(dict):
         sort_order_letters (str): Order for a sort sequence for a custom collate mode.
         config (dict): A config for a fulltext and float_vector engine.
         [More about `fulltext`](https://github.com/Restream/reindexer/blob/master/fulltext.md) or
-        [More about `float_vector`](https://github.com/Restream/reindexer/blob/master/cpp_src/float_vector.md).
+        [More about `float_vector`](https://github.com/Restream/reindexer/blob/master/float_vector.md).
     """
 
     def __getitem__(self, attr):
