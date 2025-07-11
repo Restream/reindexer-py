bafda526f8f37753d8f06c397d23f2f38c1538fb
Nikita Bushmakin <nikita.bushmakin@restream.ru>
2025-07-11 08:02:52 +0300

    [test] deploy 16 fix repack path 2

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ with open('README.md', 'r', encoding='utf-8') as file:
     LONG_DESCRIPTION = file.read()
 
 setup(name=PACKAGE_NAME,
-      version='0.5.40313',
+      version='0.5.40314',
       description='A connector that allows to interact with Reindexer. Reindexer static library or reindexer-dev package must be installed',
       author='Igor Tulmentyev',
       author_email='contactus@reindexer.io',
