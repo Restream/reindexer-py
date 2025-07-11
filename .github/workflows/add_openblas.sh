a4749ac83f71546367fa0e83c091037205f43c86
Nikita Bushmakin <nikita.bushmakin@restream.ru>
2025-07-11 07:25:43 +0300

    [test] deploy 15 fix repack path

--- .github/workflows/add_openblas.sh
+++ .github/workflows/add_openblas.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 OPENBLAS_SRC="/usr/lib64/libopenblas.so"
-WHL_FILES=$(ls wheelhouse/*.whl)
+WHL_FILES=$(ls dist/*.whl)
 while IFS= read -r whl_file; do
   unzip "$whl_file" -d unpacked
   cp "$OPENBLAS_SRC" "unpacked/pyreindexer.libs/libopenblas.so"
-
   cd unpacked
   zip -r "../$whl_file" .
   cd ..
