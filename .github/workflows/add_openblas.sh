4342c2641264a14a19fd36e1713e7a332abca57d
Nikita Bushmakin <nikita.bushmakin@restream.ru>
2025-07-11 06:57:21 +0300

    [test] deploy 14 add more openblas

---
+++ .github/workflows/add_openblas.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OPENBLAS_SRC="/usr/lib64/libopenblas.so"
+WHL_FILES=$(ls wheelhouse/*.whl)
+while IFS= read -r whl_file; do
+  unzip "$whl_file" -d unpacked
+  cp "$OPENBLAS_SRC" "unpacked/pyreindexer.libs/libopenblas.so"
+
+  cd unpacked
+  zip -r "../$whl_file" .
+  cd ..
+  rm -rf unpacked
+done <<< "$WHL_FILES"
